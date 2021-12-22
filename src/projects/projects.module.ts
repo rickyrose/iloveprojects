f2049e235dcee0bf289c0f33bee006c9d87a9023
Ricky Rose <rickyrose74@gmail.com>
2021-12-22 11:36:01 -0800

    changes

--- src/projects/projects.module.ts
+++ src/projects/projects.module.ts
@@ -1,12 +1,36 @@
-import { Module } from '@nestjs/common';
+import { Injectable, Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { Event, EventSchema } from 'src/events/entities/event.entity';
 import { Project, ProjectSchema } from './entities/project.entitiy';
+import { Stuff } from './projects.constants';
 import { ProjectsController } from './projects.controller';
 import { ProjectsService } from './projects.service';
 
+@Injectable()
+export class ProjectFactory {
+    create(){
+        //do something
+        return ['aaaa', 'bbbb', 'cccc'];
+    }
+}
+
+class ConfigService {}
+class DevelopConfigService {}
+class ProductionConfigService {}
 @Module({
     imports:[MongooseModule.forFeature([{
     name: Project.name,
     schema: ProjectSchema,
-},]),],controllers: [ProjectsController], providers: [ProjectsService]})
-export class ProjectsModule {}
+},
+{
+    name: Event.name,
+    schema:EventSchema,
+}
+]),
+],
+controllers: [ProjectsController],
+ providers:[ProjectsService,
+    ProjectFactory,
+    {    provide: Stuff, useFactory: ()=>['aaaa', 'bbbb', 'cccc'], inject: [ProjectFactory],  useClass: process.env.NODE_ENV === 'development' ? DevelopConfigService : ProjectsService,}],
+  exports:[ProjectsService]})
+export class ProjectsModule
